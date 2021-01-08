eaf79c145244f5dbce807e7a241f1a424fc86455
SilantievAleksey <aleksey_7300@mail.ru>
2021-01-09 01:37:14 +0400

    add new views

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,59 +16,10 @@
     </style>
 </head>
 <body>
+
+@include('layouts.header')
+
     <div id="app">
-
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{--{{ config('app.name', 'Laravel') }}--}}
-                    <i class="fa fa-home fa-2x" aria-hidden="true" style="color: #3490dc"></i>
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto"></ul>
-
-                    <ul class="navbar-nav ml-auto">
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->first_name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('office.main') }}">{{ __('My dispatch') }}</a>
-                                    <a class="dropdown-item" href="{{ route('profile.index') }}">{{ __('Profile') }}</a>
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
         <main class="py-4" id="main">
             <div class="container">
                 @if ($message = Session::get('success'))
@@ -83,6 +34,8 @@
             </div>
         </main>
     </div>
+
+@include('layouts.footer')
 
     <script src="{{ asset('js/app.js') }}"></script>
     @livewireScripts
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,13 @@
+<footer>
+<!--    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="container">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item"><a href="">{{ __('Feedback') }}</a></li>
+                </ul>
+
+            </div>
+        </div>
+    </nav>-->
+</footer>
